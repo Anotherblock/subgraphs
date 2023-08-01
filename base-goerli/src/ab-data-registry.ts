3baa10a87f1bcc40f2a29e8da327b3bcbf4e061d
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-08-01 14:48:49 +0200

    added DataRegistry datasource

---
+++ base-goerli/src/ab-data-registry.ts
@@ -0,0 +1,9 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  ABDataRegistry,
+  PublisherRegistered,
+  DropRegistered,
+} from "../generated/ABDataRegistry/ABDataRegistry";
+
+export function handlePublisherRegistered(event: PublisherRegistered): void {}
+export function handleDropRegistered(event: DropRegistered): void {}
